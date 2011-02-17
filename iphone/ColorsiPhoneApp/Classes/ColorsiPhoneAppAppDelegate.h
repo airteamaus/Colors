1918d5e8a3a896bd37ff58a8c6780305e339ae9f
rich <atkinsonr@gmail.com>
2011-02-17 15:36:38 +1100

    phonegap skeleton

---
+++ iphone/ColorsiPhoneApp/Classes/ColorsiPhoneAppAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  ColorsiPhoneAppAppDelegate.h
+//  ColorsiPhoneApp
+//
+//  Created by Rich Atkinson on 17/02/11.
+//  Copyright Platform46 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface ColorsiPhoneAppAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ iphone/ColorsiPhoneApp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ColorsiPhoneApp
+//
+//  Created by Rich Atkinson on 17/02/11.
+//  Copyright Platform46 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"ColorsiPhoneAppAppDelegate");
+    [pool release];
+    return retVal;
+}
