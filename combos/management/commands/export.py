f10efdfb024d1271b8d4f39de03bf9293b3f10a3
rich <atkinsonr@gmail.com>
2011-02-26 00:54:36 +1100

    new clean db

--- combos/management/commands/export.py
+++ combos/management/commands/export.py
@@ -1,10 +1,12 @@
 from django.core.management.base import BaseCommand, CommandError
 from combos.models import Combo, ClientSideColor, ClientSideCombo, ClientSideColorCombo
+from django.db import transaction
 
 class Command(BaseCommand):
     args = '<no args>'
     help = 'Generate a new SQLite clientside.db'
 
+    @transaction.commit_on_success()
     def handle(self, *args, **options):
         combos = Combo.objects.all()
         for combo in combos:
